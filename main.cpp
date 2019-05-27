63b3a457471eed1bae01afec7712f2fdf507cdd7
River <govers.river.r@gmail.com>
2019-05-27 17:37:17 +1000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ApplicationConfig.h"
+
+int main (int argc, char *argv[])
+{
+    printf("Test: %d.%d", Application_VERSION_MAJOR, Application_VERSION_MINOR);
+
+    return 0;
+}
